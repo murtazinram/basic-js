b8ab0e56399cac87bbb77fe0540b09b0c3c31fa7
murtazinram <murtazinram@gmail.com>
2020-09-28 00:00:03 +0300

    feat: need to add throw section to what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date instanceof Date){
+        const month = date.getMonth() + 1;
+        console.log(month)
+        if (month < 3 || month === 12) return 'winter'
+        if (month >= 3 && month < 6) return 'spring'
+        if (month >= 6 && month < 9) return 'summer'
+        if (month >= 9 && month < 12) return 'autumn'
+    } else {
+        return 'Unable to determine the time of year!'
+    }
 };
