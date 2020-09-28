b8723d9dff904730be2dbff625207a3ec62d5083
murtazinram <murtazinram@gmail.com>
2020-09-28 19:29:18 +0300

    feat: solved what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -1,14 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-    if (date instanceof Date){
+    if (date === undefined) {
+        return 'Unable to determine the time of year!'
+    }
+    if (Object.prototype.toString.call(date) === '[object Date]') {
         const month = date.getMonth() + 1;
-        console.log(month)
         if (month < 3 || month === 12) return 'winter'
         if (month >= 3 && month < 6) return 'spring'
         if (month >= 6 && month < 9) return 'summer'
         if (month >= 9 && month < 12) return 'autumn'
     } else {
-        return 'Unable to determine the time of year!'
+        throw new Error("bad argument")
     }
 };
+
+
