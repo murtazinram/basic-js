d1bb7dedb93d40426bebb496f591356f737d02a9
murtazinram <murtazinram@gmail.com>
2020-09-30 19:17:54 +0300

    feat: solved transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const DISCARD_NEXT = '--discard-next'
+const DISCARD_PREV = '--discard-prev'
+const DOUBLE_NEXT = '--double-next'
+const DOUBLE_PREV = '--double-prev'
+module.exports = function transform(array) {
+    let controlArray = ["--discard-next", "--discard-prev", "--double-next", "--double-prev"]
+    if (!Array.isArray(array)) throw Error;
+    let newArray = array.slice();
+    newArray.forEach(function (item, i) {
+        if (Object.is(item, DISCARD_NEXT) && i < newArray.length - 1) {
+            newArray[i + 1] = DISCARD_PREV;
+        }
+        if (Object.is(item, DISCARD_PREV) && i > 0) {
+            newArray[i - 1] = DISCARD_PREV;
+        }
+        if (Object.is(item, DOUBLE_NEXT) && i < newArray.length - 1 && controlArray.indexOf(newArray[i + 1]) === -1) {
+            newArray[i] = newArray[i + 1];
+        }
+        if (Object.is(item, DOUBLE_PREV) && i > 0 && controlArray.indexOf(newArray[i - 1]) === -1) {
+            newArray[i] = newArray[i - 1];
+        }
+    })
+    return newArray.filter(value => controlArray.indexOf(value) === -1);
 };
+
