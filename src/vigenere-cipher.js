c76a78a050bf759f7aba881ccdde999f4ad63f08
murtazinram <murtazinram@gmail.com>
2020-10-02 19:12:44 +0300

    feat: solved vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    boolean;
+
+    constructor(boolean = true) {
+        this.boolean = boolean
+    }
+
+    encrypt(message, key) {
+        if (message === undefined || key === undefined) throw new Error("message or key und")
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let str = [];
+        let arr = message.split(' ')
+        message = message.split(' ').join("")
+        while (key.length < message.length) {
+            key += key
+        }
+        key = (key.substring(0, message.length))
+        for (let i = 0; i < message.length; i++) {
+            let mesCode = message.charCodeAt(i);
+            let keyCode = key.charCodeAt(i)
+            if (mesCode >= 65 && mesCode <= 90) {
+                str.push(String.fromCharCode((mesCode + keyCode) % 26 + 65))
+            } else {
+                str.push(String.fromCharCode(mesCode))
+            }
+        }
+        let index = 0
+        for (let i = 0; i < arr.length; i++) {
+            str.splice(index, 0, ' ')
+            index = index + arr[i].length + 1
+        }
+        str = str.join('').trim()
+        return this.boolean ? str : str.split("").reverse().join("")
+    }
+
+    decrypt(message, key) {
+        if (message === undefined || key === undefined) throw new Error("message or key und")
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let str = [];
+        let arr = message.split(' ')
+        message = message.split(' ').join("")
+        while (key.length < message.length) {
+            key += key
+        }
+        key = (key.substring(0, message.length))
+        for (let i = 0; i < message.length; i++) {
+            let mesCode = message.charCodeAt(i);
+            let keyCode = key.charCodeAt(i)
+            if (mesCode >= 65 && mesCode <= 90) {
+                str.push(String.fromCharCode((mesCode - keyCode + 26) % 26 + 65))
+            } else {
+                str.push(String.fromCharCode(mesCode))
+            }
+        }
+        let index = 0
+        for (let i = 0; i < arr.length; i++) {
+            str.splice(index, 0, ' ')
+            index = index + arr[i].length + 1
+        }
+        str = str.join('').trim()
+        return this.boolean ? str : str.split("").reverse().join("")
+    }
 }
 
 module.exports = VigenereCipheringMachine;
