337f9e1ee6ef24eb37d726ee3699212ca1d2d6cb
murtazinram <murtazinram@gmail.com>
2020-10-02 19:44:42 +0300

    refactor: vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,35 +8,14 @@ class VigenereCipheringMachine {
     }
 
     encrypt(message, key) {
-        if (message === undefined || key === undefined) throw new Error("message or key und")
-        message = message.toUpperCase();
-        key = key.toUpperCase();
-        let str = [];
-        let arr = message.split(' ')
-        message = message.split(' ').join("")
-        while (key.length < message.length) {
-            key += key
-        }
-        key = (key.substring(0, message.length))
-        for (let i = 0; i < message.length; i++) {
-            let mesCode = message.charCodeAt(i);
-            let keyCode = key.charCodeAt(i)
-            if (mesCode >= 65 && mesCode <= 90) {
-                str.push(String.fromCharCode((mesCode + keyCode) % 26 + 65))
-            } else {
-                str.push(String.fromCharCode(mesCode))
-            }
-        }
-        let index = 0
-        for (let i = 0; i < arr.length; i++) {
-            str.splice(index, 0, ' ')
-            index = index + arr[i].length + 1
-        }
-        str = str.join('').trim()
-        return this.boolean ? str : str.split("").reverse().join("")
+        return this.extracted(message, key, true);
     }
 
     decrypt(message, key) {
+        return this.extracted(message, key, false)
+    }
+
+    extracted(message, key, mode) {
         if (message === undefined || key === undefined) throw new Error("message or key und")
         message = message.toUpperCase();
         key = key.toUpperCase();
@@ -51,7 +30,11 @@ class VigenereCipheringMachine {
             let mesCode = message.charCodeAt(i);
             let keyCode = key.charCodeAt(i)
             if (mesCode >= 65 && mesCode <= 90) {
-                str.push(String.fromCharCode((mesCode - keyCode + 26) % 26 + 65))
+                if (mode) {
+                    str.push(String.fromCharCode((mesCode + keyCode) % 26 + 65))
+                } else {
+                    str.push(String.fromCharCode((mesCode - keyCode + 26) % 26 + 65))
+                }
             } else {
                 str.push(String.fromCharCode(mesCode))
             }
